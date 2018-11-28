0fcf4e489dea656d7a6d25e59347ff2a5b865cd1
FOrtiz <forzzak@gmail.com>
2018-11-27 21:13:09 -0400

    Add data and fix flu keyword

--- DataGathering/p3BD.py
+++ DataGathering/p3BD.py
@@ -5,4 +5,4 @@ spark = SparkSession.builder.getOrCreate()
 
 df = spark.read.json('/home/Ferni/BigDataTweets.txt')
 df.createOrReplaceTempView('tweets')
-spark.sql("select lower(extended_tweet.full_text) as tweetText from tweets where lower(extended_tweet.full_text) != 'null' and (lower(extended_tweet.full_text) like '%flu%' or lower(extended_tweet.full_text) like '%zika%' or lower(extended_tweet.full_text) like '%diarrhea%' or lower(extended_tweet.full_text) like '%ebola%' or lower(extended_tweet.full_text) like '%headache%' or lower(extended_tweet.full_text) like '%measles%')").coalesce(1).write.csv('/home/Ferni/data/p3/TweetsContainingKeywords')
+spark.sql("select lower(extended_tweet.full_text) as tweetText from tweets where lower(extended_tweet.full_text) != 'null' and (lower(extended_tweet.full_text) like '%flu %' or lower(extended_tweet.full_text) like '%zika%' or lower(extended_tweet.full_text) like '%diarrhea%' or lower(extended_tweet.full_text) like '%ebola%' or lower(extended_tweet.full_text) like '%headache%' or lower(extended_tweet.full_text) like '%measles%')").coalesce(1).write.csv('/home/Ferni/data/p3/TweetsContainingKeywords')
